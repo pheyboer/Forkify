b48ab1d57784db0229518113e92aeb0419d6dc91
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-03 16:14:33 -0500

    implementing pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -42,8 +42,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //render results
-
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
   } catch (err) {
     console.log(err);
   }
