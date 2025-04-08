d482b8ab09847f99f09d465d0432a9bde7e75b39
pheyboer <heyboer.peter@gmail.com>
2025-04-08 15:56:19 -0400

    implementing new recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,7 +6,6 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
-
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
